c657123f9c2a16b4552e8f7d6f1ed51acf1da422
Harsimarjit <simarbadding@gmail.com>
2024-02-28 20:35:09 -0500

    added a basic homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import img2 from "./assets/my-image3.jpg"
 
 function App() {
 
   return (
     <>
-    <div className='text-3xl'>Portfolio</div>
+    <div className='bg-gray-100 h-screen'> 
+      <div className='flex justify-around p-20'>
+        <div className='bg-gray-200 py-10 px-5'>
+          <h3 className='text-3xl'>Harsimarjit Singh</h3>
+          <p>I am a Software Enginner with passion for poblem solving with web development.</p>
+        </div>
+        <img className='size-96 rounded-3xl bg-red-500' src={img2} alt="Simar's image"/>
+      </div>
+    </div>
     </>
   )
 }
